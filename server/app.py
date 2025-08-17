70461b7fb5b978686fe5f577103427db37aba619
Sahil Omkumar Dhillon <118592065+SahilDhillon21@users.noreply.github.com>
2025-08-17 14:42:50 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -20,7 +20,7 @@ DB_CONFIG = {
 latest_data = {"temperature": None, "humidity": None, "latitude": None, "longitude": None, "package_id": None, "timestamp": None}
 
 def get_db_connection():
-    """Hiii"""
+    """Hiii there 1"""
     try:
         """Create and return the connection"""
         conn = psycopg2.connect(**DB_CONFIG)
