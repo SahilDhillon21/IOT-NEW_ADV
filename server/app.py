ae1e3e315b8c6e320d0039e02b830de1822ae28a
Sahil Omkumar Dhillon <118592065+SahilDhillon21@users.noreply.github.com>
2025-09-02 22:17:26 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -18,11 +18,9 @@ DB_CONFIG = {
 latest_data = {"temperature": None, "humidity": None, "latitude": None, "longitude": None, "timestamp": None}
 
 def get_db_connection():
-    """A new poitnless comment"""
+    """New comment"""
     try:
         """Create and return the connection, maybe"""
-        conn = psycopg2.connect(**DB_CONFIG) # okay
-        return conn
     except Exception as e:
         print(f"Database connection error: {e}")
         return None
